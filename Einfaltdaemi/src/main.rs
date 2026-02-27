35c630798f208a5c71c87a3890ee885919efb39f
unknown <mattimadsen@gmail.com>
2026-02-27 17:45:34 +0000

    klaradi einfalt dæmi i python, gat ekki í rust :(

--- Einfaltdaemi/src/main.rs
+++ Einfaltdaemi/src/main.rs
@@ -3,9 +3,15 @@ use std::io;
 fn main() {
     let mut input = String::new();
     
-    io::stdin()
-        .read_line(&mut input)
-        .unwrap();
+    io::stdin().read_line(&mut input).unwrap();
     
-    println!("{}", input);
+    let n: i128 = input.trim().parse().unwrap();
+
+    let mut n_hropmerkt: i128 = 1;
+
+    for i in 1..=n {
+        n_hropmerkt *= i;
+    }
+
+    println!("{}", n_hropmerkt);
 }
