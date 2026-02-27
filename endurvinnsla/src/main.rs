bdfe39052b0a8ae317800c29e8f08f84e67e2cae
unknown <mattimadsen@gmail.com>
2026-02-27 17:01:37 +0000

    endurvinnsla klárað á undan Gabríel m/ aura. Kóði getur verið bættur

--- endurvinnsla/src/main.rs
+++ endurvinnsla/src/main.rs
@@ -5,18 +5,33 @@ fn main() {
     io::stdin().read_line(&mut input).unwrap();
     
     input.clear();
-    io::stdin().readline(&mut input).unwrap();
+    io::stdin().read_line(&mut input).unwrap();
     
     let p: f32 = input.trim().parse().unwrap();
     
     input.clear();
+    io::stdin().read_line(&mut input).unwrap();
     let n: i32 = input.trim().parse().unwrap();
     
-    let mut plast_fjoldi = 0
+    let mut almennt_sorp: f32 = 0.0;
+
     for _ in 0..n {
-        
+        input.clear();
+        io::stdin().read_line(&mut input).unwrap();
+
+        if input.trim() == "ekki plast" {
+            almennt_sorp += 1.0;
+        }
     }
+
+    let plast_hlutfall: f32 = (almennt_sorp / n as f32);
+
+    if p >= plast_hlutfall {
+        println!("Jebb")
+    } else {
+        println!("Neibb")
+    }
+    
     //I <3 you, Eldur!
-    
-    
+
 }
