e783474879ce24727f8d80dcb52d44f08be74331
mattimeistari <mattimadsen@gmail.com>
2026-02-27 16:20:41 +0000

    Byrjadi a endurvinnslu verkefni

---
+++ endurvinnsla/src/main.rs
@@ -0,0 +1,22 @@
+use std::io;
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    
+    input.clear();
+    io::stdin().readline(&mut input).unwrap();
+    
+    let p: f32 = input.trim().parse().unwrap();
+    
+    input.clear();
+    let n: i32 = input.trim().parse().unwrap();
+    
+    let mut plast_fjoldi = 0
+    for _ in 0..n {
+        
+    }
+    //I <3 you, Eldur!
+    
+    
+}
